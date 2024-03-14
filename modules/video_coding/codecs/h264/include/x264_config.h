aa0953e3a3cff37f0c8c454e44bd609c8b6eac6f
hxjwb <newasdsd@163.com>
2024-03-14 13:12:20 +0800

    Testing: replace openh264 with libx264

---
+++ modules/video_coding/codecs/h264/include/x264_config.h
@@ -0,0 +1,6 @@
+#define X264_GPL           1
+#define X264_INTERLACED    1
+#define X264_BIT_DEPTH     0
+#define X264_CHROMA_FORMAT 0
+#define X264_VERSION " r3095 baee400"
+#define X264_POINTVER "0.164.3095 baee400"
