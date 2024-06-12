a597633ae405a88e1d2e2f968af1c401204a3d55
hxjwb <newasdsd@163.com>
2024-06-12 15:38:14 +0800

    Fixed missing main_mac.cc for MacOS

---
+++ examples/peerconnection/localvideo/main_mac.cc
@@ -0,0 +1,139 @@
+/*
+ *  Copyright 2012 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+
+#include <stdio.h>
+
+#include "absl/flags/parse.h"
+#include "api/scoped_refptr.h"
+#include "examples/peerconnection/localvideo/conductor.h"
+#include "examples/peerconnection/localvideo/flag_defs.h"
+#include "examples/peerconnection/localvideo/linux/main_wnd.h"
+#include "examples/peerconnection/localvideo/linux/fake_wnd.h"
+#include "examples/peerconnection/localvideo/peer_connection_localvideo.h"
+#include "rtc_base/physical_socket_server.h"
+#include "rtc_base/ssl_adapter.h"
+#include "rtc_base/thread.h"
+#include "system_wrappers/include/field_trial.h"
+#include "test/field_trial.h"
+
+std::string local_video_filename;
+std::string recon_filename;
+int local_video_width;
+int local_video_height;
+int local_video_fps;
+bool is_sender = false;
+bool is_GUI = false;
+
+class CustomSocketServerNoGUi : public rtc::PhysicalSocketServer {
+ public:
+  explicit CustomSocketServerNoGUi(FakeMainWnd* wnd)
+      : wnd_(wnd), conductor_(NULL), client_(NULL) {}
+  virtual ~CustomSocketServerNoGUi() {}
+
+  void SetMessageQueue(rtc::Thread* queue) override { message_queue_ = queue; }
+
+  void set_client(PeerConnectionClient* client) { client_ = client; }
+  void set_conductor(Conductor* conductor) { conductor_ = conductor; }
+
+  // Override so that we can also pump the GTK message loop.
+  // This function never waits.
+
+  bool Wait(webrtc::TimeDelta max_wait_duration, bool process_io) override {
+    // RTC_LOG(LS_INFO) << __FUNCTION__ ;
+    // Pump GTK events.
+    // TODO(henrike): We really should move either the socket server or UI to a
+    // different thread.  Alternatively we could look at merging the two loops
+    // by implementing a dispatcher for the socket server and/or use
+    // g_main_context_set_poll_func.
+    //  int64_t start_ts = rtc::TimeMillis();
+    // RTC_LOG(LS_INFO) << __FUNCTION__ << " Wait: " << start_ts;
+    usleep(50000);
+    return rtc::PhysicalSocketServer::Wait(webrtc::TimeDelta::Zero(),
+                                           process_io);
+  }
+
+ protected:
+  rtc::Thread* message_queue_;
+  FakeMainWnd* wnd_;
+  Conductor* conductor_;
+  PeerConnectionClient* client_;
+};
+
+int main(int argc, char* argv[]) {
+  absl::ParseCommandLine(argc, argv);
+
+  // InitFieldTrialsFromString stores the char*, so the char array must outlive
+  // the application.
+  const std::string forced_field_trials =
+      absl::GetFlag(FLAGS_force_fieldtrials);
+  webrtc::field_trial::InitFieldTrialsFromString(forced_field_trials.c_str());
+
+  // Abort if the user specifies a port that is outside the allowed
+  // range [1, 65535].
+  if ((absl::GetFlag(FLAGS_port) < 1) || (absl::GetFlag(FLAGS_port) > 65535)) {
+    printf("Error: %i is not a valid port.\n", absl::GetFlag(FLAGS_port));
+    return -1;
+  }
+
+  const std::string server = absl::GetFlag(FLAGS_server);
+
+  local_video_filename = absl::GetFlag(FLAGS_file);
+  recon_filename = absl::GetFlag(FLAGS_recon);
+  local_video_width = absl::GetFlag(FLAGS_width);
+  local_video_height = absl::GetFlag(FLAGS_height);
+  local_video_fps = absl::GetFlag(FLAGS_fps);
+  is_GUI = absl::GetFlag(FLAGS_gui);
+  
+
+  bool autocall = false;
+  if (local_video_filename != "NONE") {
+    is_sender = true;
+    autocall = true;
+  }
+
+  if (is_GUI) {
+    // Not supported yet.
+    fprintf(stderr, "Error: --gui not supported on MacOS.\n");
+    return -1;
+  }
+  else{
+    FakeMainWnd wnd(server.c_str(), absl::GetFlag(FLAGS_port),
+                   absl::GetFlag(FLAGS_autoconnect), autocall);
+    wnd.Create();
+
+    CustomSocketServerNoGUi socket_server(&wnd);
+    rtc::AutoSocketServerThread thread(&socket_server);
+
+    rtc::InitializeSSL();
+    // Must be constructed after we set the socketserver.
+    PeerConnectionClient client;
+    auto conductor = rtc::make_ref_counted<Conductor>(&client, &wnd);
+    socket_server.set_client(&client);
+    socket_server.set_conductor(conductor.get());
+
+    wnd.AutoLogin();
+
+    thread.Run();
+
+    // gtk_main();
+    wnd.Destroy();
+
+    // TODO(henrike): Run the Gtk main loop to tear down the connection.
+    /*
+    while (gtk_events_pending()) {
+      gtk_main_iteration();
+    }
+    */
+    rtc::CleanupSSL();
+  }
+
+  return 0;
+}
